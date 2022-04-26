8cece18fa4b11ec40ad351ee23611f3bd8466439
intelltest <akdlakdrn@naver.com>
2022-04-26 14:43:28 +0900

    깃 허브 추가 테스트 작업

--- src/main/java/com/firstgit/git/GitApplication.java
+++ src/main/java/com/firstgit/git/GitApplication.java
@@ -10,6 +10,7 @@ public class GitApplication {
         SpringApplication.run(GitApplication.class, args);
         System.out.println("프로젝트 생성완료");
         System.out.println("깃테스트 연동하는중");
+        System.out.println("깃 테스트 추가 작업 완료");
     }
 
 }
